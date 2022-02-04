8b0d90bff21c9070a3badf9e2ea4518d481e64bf
Matias Koivumaa <matiaskoivumaa@gmail.com>
2022-02-04 16:01:35 +0200

    search field working

--- lib/books.dart
+++ lib/books.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main.dart';
 
 class Books extends StatelessWidget {
-  final List<Map<String, Object>> books;
+  final List<Book> books;
 
-  Books(@required this.books);
+  Books(this.books);
 
-  List<Image> get images {
+  List<Image> get image {
     var images = <Image>[];
     for (int i=0; i<books.length; i++) {
-      images.add(books[i]["image"]);
+      images.add(books[i].image);
     }
     return images;
   }
 
+  /*List<Text> get name {
+    var names = <Text>[];
+    for (int i = 0; i < books.length; i++) {
+      names.add(Text(
+        books[i].name,
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ));
+    }
+    return names;
+  }*/
+
   @override
   Widget build(BuildContext context) {
-    return (Expanded(
-      child: GridView.count(
+    return GridView.count(
         crossAxisCount: 2,
         mainAxisSpacing: 20,
-        children: images,
-      ),
-    ));
+        children: image,
+    );
   }
 }
